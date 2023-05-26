60e5ff6422bccf56a518a6096da67bd9f563105c
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2023-05-26 15:52:34 -0300

    feat: fixing typing error in getFieldPriority

--- capputeeno/src/utils/graphql-filters.ts
+++ capputeeno/src/utils/graphql-filters.ts
@@ -11,9 +11,9 @@ export function getFieldPriority(priority: PriorityTypes) {
   if (priority === PriorityTypes.NEWS)
     return { field: "created_at", order: "ASC" };
   if (priority === PriorityTypes.BIGGEST_PRICE)
-    return { field: "prince_in_cents", order: "ASC" };
+    return { field: "price_in_cents", order: "ASC" };
   if (priority === PriorityTypes.MINOR_PRICE)
-    return { field: "prince_in_cents", order: "DSC" };
+    return { field: "price_in_cents", order: "DSC" };
 
   return { field: "sales", order: "DSC" };
 }
@@ -38,13 +38,12 @@ export const mountQuery = (type: FilterType, priority: PriorityTypes) => {
   query {
       allProducts(sortField : "${sortSettings.field}", sortOrder : "${
     sortSettings.order
-  }",   ${categoryFilter ? `filter: {category: "${categoryFilter}"}` : ""}
-
+  }", ${categoryFilter ? `filter: {category: "${categoryFilter}"}` : ""}
   ) {
-          id
-          name
-          price_in_cents
-          image_url
-      }
+        id
+        name
+        price_in_cents
+        image_url
+    }
   }`;
 };
