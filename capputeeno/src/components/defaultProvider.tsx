06048ece5308d6d00fcaed35eb08d19f6e643253
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2023-05-30 17:12:54 -0300

    refactor: extract providers

---
+++ capputeeno/src/components/defaultProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import FilterContextProvider from "@/contexts/filter-context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { ThemeProvider } from "styled-components";
+
+interface DefaultProps {
+  children: ReactNode;
+}
+
+const theme = {
+  desktopBreakpoint: "980px",
+};
+
+export default function DefaultProvider({ children }: DefaultProps) {
+  const client = new QueryClient();
+
+  return (
+    <div>
+      <QueryClientProvider client={client}>
+        <FilterContextProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </FilterContextProvider>
+      </QueryClientProvider>
+    </div>
+  );
+}
